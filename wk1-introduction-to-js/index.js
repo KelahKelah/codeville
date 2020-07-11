b03a2b5f5d5c7907e4d8e8617785cf7a7664823a
KelahKelah <kelechiekebor@gmail.com>
2020-07-11 23:34:05 +0100

    add week 2 and week 3

---
+++ wk1-introduction-to-js/index.js
@@ -0,0 +1,35 @@
+// DATA TYPES VARIABLE IN JAVASCRIPT
+
+// Programming is all about manipulating Data. What is data? Data is simply the way we store information on our computer. 
+// Data in javascript is grouped in 3 different form. We use to var keyword to declare a data type in javascript
+
+// Numbers 273
+// String "I am a string"
+// Boolean True/False
+
+var myNumber = 726;
+var myString = "I am a string";
+var boolean = true;
+
+window.alert('js is working')
+
+// LOOPS 
+// Loops allow you perform as a task repitedly. For instance you can draw as many cats as you want 
+// var cat = ['=^.^=']
+
+// var myFunc = function(many) {
+//     for(var i=0; cat.length > 0; i++) {
+//         cat[i]
+//     }
+// }
+
+// console.log(myFunc())
+
+
+ function drawCat(howManyTimes) {
+    for (var i = 0; i < howManyTimes; i++) {
+    console.log(i + " =^.^=");
+    }
+   };
+
+   
